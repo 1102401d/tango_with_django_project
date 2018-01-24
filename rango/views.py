30d1b8dd9f6910af149d7946d6b032696d070c6c
Jamie Dickson <1102401d@student.gla.ac.uk>
2018-01-24 14:17:07 +0000

    fix chapter 3 tests

--- rango/views.py
+++ rango/views.py
@@ -5,4 +5,4 @@ def index(request):
     return HttpResponse("Rango says hey there partner! <br/><a href='rango/about/'>About</a>")
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <br/><a href='http://127.0.0.1:8000'>Main page</a>")
+    return HttpResponse("Rango says here is the about page. <br/><a href='/rango/'>Index</a>")
