b832ee9ec0e43432fced12c7947e1751bd4abf73
Jamie Dickson <1102401d@student.gla.ac.uk>
2018-02-13 15:57:29 +0000

    fixed spelling mistake halfway through chap 11

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -133,3 +133,9 @@ MEDIA_URL = '/media/'
 
 
 LOGIN_URL = '/rango/login/'
+
+REGISTRATION_OPEN = True
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = '/rango/'
+LOGIN_URL = '/rango/login/'
